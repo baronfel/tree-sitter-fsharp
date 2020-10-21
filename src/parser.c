3bc407bc2d6959169dbf13c178835796d2805ffc
Chet Husk <chusk3@gmail.com>
2020-10-20 19:18:35 -0500

    add initial generated artifacts

---
+++ src/parser.c
@@ -0,0 +1,183 @@
+#include <tree_sitter/parser.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
+
+#define LANGUAGE_VERSION 12
+#define STATE_COUNT 4
+#define LARGE_STATE_COUNT 2
+#define SYMBOL_COUNT 3
+#define ALIAS_COUNT 0
+#define TOKEN_COUNT 2
+#define EXTERNAL_TOKEN_COUNT 0
+#define FIELD_COUNT 0
+#define MAX_ALIAS_SEQUENCE_LENGTH 1
+
+enum {
+  aux_sym_comment_token1 = 1,
+  sym_comment = 2,
+};
+
+static const char *ts_symbol_names[] = {
+  [ts_builtin_sym_end] = "end",
+  [aux_sym_comment_token1] = "comment_token1",
+  [sym_comment] = "comment",
+};
+
+static TSSymbol ts_symbol_map[] = {
+  [ts_builtin_sym_end] = ts_builtin_sym_end,
+  [aux_sym_comment_token1] = aux_sym_comment_token1,
+  [sym_comment] = sym_comment,
+};
+
+static const TSSymbolMetadata ts_symbol_metadata[] = {
+  [ts_builtin_sym_end] = {
+    .visible = false,
+    .named = true,
+  },
+  [aux_sym_comment_token1] = {
+    .visible = false,
+    .named = false,
+  },
+  [sym_comment] = {
+    .visible = true,
+    .named = true,
+  },
+};
+
+static TSSymbol ts_alias_sequences[1][MAX_ALIAS_SEQUENCE_LENGTH] = {
+  [0] = {0},
+};
+
+static uint16_t ts_non_terminal_alias_map[] = {
+  0,
+};
+
+static bool ts_lex(TSLexer *lexer, TSStateId state) {
+  START_LEXER();
+  eof = lexer->eof(lexer);
+  switch (state) {
+    case 0:
+      if (eof) ADVANCE(5);
+      if (lookahead == '(') ADVANCE(1);
+      if (lookahead == '/') ADVANCE(2);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') SKIP(0)
+      END_STATE();
+    case 1:
+      if (lookahead == '*') ADVANCE(3);
+      END_STATE();
+    case 2:
+      if (lookahead == '/') ADVANCE(7);
+      END_STATE();
+    case 3:
+      if (lookahead != 0 &&
+          lookahead != '*') ADVANCE(3);
+      if (lookahead == '*') ADVANCE(4);
+      END_STATE();
+    case 4:
+      if (lookahead != 0 &&
+          lookahead != ')' &&
+          lookahead != '*') ADVANCE(3);
+      if (lookahead == ')') ADVANCE(6);
+      if (lookahead == '*') ADVANCE(4);
+      END_STATE();
+    case 5:
+      ACCEPT_TOKEN(ts_builtin_sym_end);
+      END_STATE();
+    case 6:
+      ACCEPT_TOKEN(aux_sym_comment_token1);
+      if (lookahead != 0 &&
+          lookahead != '*') ADVANCE(3);
+      if (lookahead == '*') ADVANCE(4);
+      END_STATE();
+    case 7:
+      ACCEPT_TOKEN(aux_sym_comment_token1);
+      if (lookahead != 0 &&
+          lookahead != '\n' &&
+          lookahead != '\r') ADVANCE(7);
+      END_STATE();
+    default:
+      return false;
+  }
+}
+
+static TSLexMode ts_lex_modes[STATE_COUNT] = {
+  [0] = {.lex_state = 0},
+  [1] = {.lex_state = 0},
+  [2] = {.lex_state = 0},
+  [3] = {.lex_state = 0},
+};
+
+static uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
+  [0] = {
+    [ts_builtin_sym_end] = ACTIONS(1),
+    [aux_sym_comment_token1] = ACTIONS(1),
+  },
+  [1] = {
+    [sym_comment] = STATE(3),
+    [aux_sym_comment_token1] = ACTIONS(3),
+  },
+};
+
+static uint16_t ts_small_parse_table[] = {
+  [0] = 1,
+    ACTIONS(5), 1,
+      ts_builtin_sym_end,
+  [4] = 1,
+    ACTIONS(7), 1,
+      ts_builtin_sym_end,
+};
+
+static uint32_t ts_small_parse_table_map[] = {
+  [SMALL_STATE(2)] = 0,
+  [SMALL_STATE(3)] = 4,
+};
+
+static TSParseActionEntry ts_parse_actions[] = {
+  [0] = {.entry = {.count = 0, .reusable = false}},
+  [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
+  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
+  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_comment, 1),
+  [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef _WIN32
+#define extern __declspec(dllexport)
+#endif
+
+extern const TSLanguage *tree_sitter_fsharp(void) {
+  static TSLanguage language = {
+    .version = LANGUAGE_VERSION,
+    .symbol_count = SYMBOL_COUNT,
+    .alias_count = ALIAS_COUNT,
+    .token_count = TOKEN_COUNT,
+    .large_state_count = LARGE_STATE_COUNT,
+    .alias_map = ts_non_terminal_alias_map,
+    .state_count = STATE_COUNT,
+    .symbol_metadata = ts_symbol_metadata,
+    .parse_table = (const unsigned short *)ts_parse_table,
+    .small_parse_table = (const uint16_t *)ts_small_parse_table,
+    .small_parse_table_map = (const uint32_t *)ts_small_parse_table_map,
+    .parse_actions = ts_parse_actions,
+    .lex_modes = ts_lex_modes,
+    .symbol_names = ts_symbol_names,
+    .public_symbol_map = ts_symbol_map,
+    .alias_sequences = (const TSSymbol *)ts_alias_sequences,
+    .field_count = FIELD_COUNT,
+    .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
+    .lex_fn = ts_lex,
+    .external_token_count = EXTERNAL_TOKEN_COUNT,
+  };
+  return &language;
+}
+#ifdef __cplusplus
+}
+#endif
